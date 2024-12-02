1ba342ed7b67a97486f4cdcb956aa941d1b695ae
nicksanjaya <nicky.sanjaya@gmail.com>
2024-12-02 15:49:53 +0700

    Update nidut.py

--- nidut.py
+++ nidut.py
@@ -25,7 +25,7 @@ def main():
     harga_telur = 13000 
     telur_min = 50
     harga_special = 15000 
-    special_min = 50 
+    special_min = Null 
 
     with st.container():
         col_k = st.columns(1)
